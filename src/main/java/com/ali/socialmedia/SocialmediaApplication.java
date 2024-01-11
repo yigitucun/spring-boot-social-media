052cdd6187715836d84fa253b834ed1e57d54807
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2024-01-11 19:09:32 +0300

    fix white space

--- src/main/java/com/ali/socialmedia/SocialmediaApplication.java
+++ src/main/java/com/ali/socialmedia/SocialmediaApplication.java
@@ -1,13 +1,19 @@
 package com.ali.socialmedia;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SocialmediaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialmediaApplication.class, args);
+	}
+	@Bean
+	public ModelMapper getModelMapper(){
+		return new ModelMapper();
 	}
 
 }
