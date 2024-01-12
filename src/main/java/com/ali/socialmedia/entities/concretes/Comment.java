72acdcf4505ea4d03148be37a51d2c061ea120d1
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2024-01-12 23:17:02 +0300

    Create Comment.java

---
+++ src/main/java/com/ali/socialmedia/entities/concretes/Comment.java
@@ -0,0 +1,42 @@
+package com.ali.socialmedia.entities.concretes;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "comments")
+public class Comment extends BaseEntity {
+    private String comment;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public Comment() {
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+}
