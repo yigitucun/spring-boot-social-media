195f5b64a4eb61ff81ea46fe26003213d91a02bb
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2024-01-16 00:18:05 +0300

    Create TokenResponse.java

---
+++ src/main/java/com/ali/socialmedia/core/dto/responses/TokenResponse.java
@@ -0,0 +1,16 @@
+package com.ali.socialmedia.core.dto.responses;
+
+public class TokenResponse {
+    private String token;
+
+    public TokenResponse() {
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
