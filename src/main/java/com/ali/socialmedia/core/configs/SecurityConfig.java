6250439e05f4f018870629202c9ccc339a2d90d1
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2024-01-15 15:20:59 +0300

    Update SecurityConfig.java

--- src/main/java/com/ali/socialmedia/core/configs/SecurityConfig.java
+++ src/main/java/com/ali/socialmedia/core/configs/SecurityConfig.java
@@ -1,15 +1,31 @@
 package com.ali.socialmedia.core.configs;
 
+import com.ali.socialmedia.core.filters.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    private final CustomUserDetailService userDetailService;
+    private final JwtAuthFilter authFilter;
+
+    public SecurityConfig(CustomUserDetailService userDetailService, JwtAuthFilter authFilter) {
+        this.userDetailService = userDetailService;
+        this.authFilter = authFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable);
@@ -17,7 +33,24 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize->authorize
                         .requestMatchers("/**").permitAll()
                         .anyRequest().permitAll()
-                );
+                )
+                .addFilterBefore(this.authFilter, UsernamePasswordAuthenticationFilter.class)
+                .authenticationProvider(this.provider());
         return http.build();
+    }
+    @Bean
+    public AuthenticationProvider provider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(this.encoder());
+        authenticationProvider.setUserDetailsService(this.userDetailService);
+        return authenticationProvider;
+    }
+    @Bean
+    public AuthenticationManager manager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
     }
 }
