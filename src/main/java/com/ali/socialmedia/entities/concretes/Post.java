07e41f4c5b58bace2a742f1f52b950c57c8aa0d6
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2024-01-23 16:56:07 +0300

    Update Post.java

--- src/main/java/com/ali/socialmedia/entities/concretes/Post.java
+++ src/main/java/com/ali/socialmedia/entities/concretes/Post.java
@@ -1,22 +1,33 @@
 package com.ali.socialmedia.entities.concretes;
 
 import jakarta.persistence.*;
+
+import java.io.Serializable;
 import java.util.Set;
 
 @Entity
 @Table(name = "posts")
-public class Post extends BaseEntity {
+public class Post extends BaseEntity implements Serializable {
     private String title;
     private String description;
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "post_likes",
         joinColumns = @JoinColumn(name = "post_id"),
         inverseJoinColumns = @JoinColumn(name = "user_id")
     )
     private Set<User> likes;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+    private String postImageUrl;
+
+    public String getPostImageUrl() {
+        return postImageUrl;
+    }
+
+    public void setPostImageUrl(String postImageUrl) {
+        this.postImageUrl = postImageUrl;
+    }
 
     public Post() {
     }
