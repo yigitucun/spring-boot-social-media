c7dd6dc8f16a24203864c544b40e13ab50d40381
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2024-01-23 16:54:31 +0300

    Create UpdateProfileRequest.java

---
+++ src/main/java/com/ali/socialmedia/core/dto/requests/UpdateProfileRequest.java
@@ -0,0 +1,49 @@
+package com.ali.socialmedia.core.dto.requests;
+
+public class UpdateProfileRequest {
+    private int id;
+    private String location;
+    private String bio;
+    private String profilePhotoUrl;
+    private int userId;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getProfilePhotoUrl() {
+        return profilePhotoUrl;
+    }
+
+    public void setProfilePhotoUrl(String profilePhotoUrl) {
+        this.profilePhotoUrl = profilePhotoUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
